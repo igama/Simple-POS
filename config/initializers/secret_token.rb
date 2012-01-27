3f20992b4487c58c70d507eec68dfbad19d1429e
Marco Silva <igamads@gmail.com>
2012-01-27 15:07:50 +0100

    It starts

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Phoneshop::Application.config.secret_token = 'f53ba3e5b663260e5af30616150da544e133fbfeae6a7c62560243f36c225926b50f9028c6c2201cf5d348eacc56a83e5d86f39dac227c573c946d681d47fcdd'
