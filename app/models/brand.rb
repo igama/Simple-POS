8d33046214ac937957a5e3d0f6ad3414b873c863
Marco Silva <igamads@gmail.com>
2012-01-27 18:53:38 +0100

    Validation added

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,3 +1,7 @@
 class Brand < ActiveRecord::Base
   has_many :products
+  
+  validates :name, :presence => true
+  validates :name, :uniqueness => true
+  
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,14 @@
 class Product < ActiveRecord::Base
+  # Relations
   belongs_to :brand
   
+  # Validations
+  #validates :name, :price, :presence => true
+  #validates :price, :numericality => {:greater_than_or_equal_to => 0.00}
+  #validates :name, :uniqueness => true
+  
   CATEGORY_TYPES = [ "Mobile", "Tablet" ]
+  
+  
   
 end
