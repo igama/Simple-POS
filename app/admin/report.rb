6336446264ddb107a6438bd9a0aee4da8e1f7bd2
Marco Silva <igamads@gmail.com>
2012-02-23 15:12:44 +0100

    Improved layout with bootstrap

--- app/admin/report.rb
+++ app/admin/report.rb
@@ -1,16 +1,17 @@
 ActiveAdmin.register_page "Report" do
 	#menu :label => "My Menu Item Label", :parent => "Dashboard"
 
+      @TopProducts = ProductOrderQuantity.limit(10)
+      @orders = Order.where('created_at < ? and created_at > ?', Time.now, 1.week.ago)
+      
+
   	action_item do
       link_to "View Site", "/"
     end
 
     content do
-      @TopProducts = ProductOrderQuantity.limit(10)
-      @orders = Order.where('created_at < ? and created_at > ?', Time.now, 1.week.ago)
       @something = "hello marco"
-
-      render "test"
+      render :partial => "test", :locals => {:something => @something}
 
       #panel "Hello World" do
       #  "Why so easy?"
